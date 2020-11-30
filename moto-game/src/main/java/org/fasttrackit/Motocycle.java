dbec4311b5da1eb7065441ef5a0b4eb4b939d350
NatyOrszag <orszagnatanael@gmail.com>
2020-11-30 16:29:54 +0200

    Moto Game

---
+++ moto-game/src/main/java/org/fasttrackit/Motocycle.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class Motocycle {
+    String name;
+    String model;
+    String color;
+    double mileage;
+    double fuellevel;
+    double maxSpeed;
+    double totalTravelDistance;
+    boolean damaged;
+}
